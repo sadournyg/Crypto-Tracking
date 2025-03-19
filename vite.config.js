42ef7891b7ac20e05b8759e35c545dc289fc3d39
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-03-19 12:15:03 +0100

    fix: deployement API key

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
