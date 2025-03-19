e8aa1710613eb4356ef3d055135073d35cf98462
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-03-19 09:47:10 +0100

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import CoinTable from "./Components/CoinTable";
-
+import Footer from "./Components/Footer";
 const App = () => {
   return (
     <div className="bg-[#2e0a91] text-white">
       {/* <Navbar /> */}
       <CoinTable />
+      <Footer />
     </div>
   );
 };
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="text-center mx-auto max-w-[80%] border-t border-gray-500 py-4 text-sm">
+      <p>Copyright 2025, CryptoCheck - All Rights Reserved</p>
+    </div>
+  );
 };
 
 export default Footer;
